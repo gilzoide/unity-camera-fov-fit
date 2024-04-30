8594b5c57f52c790f064782105aedc4c5586137f
gilzoide <gilzoide@gmail.com>
2024-04-30 08:20:13 -0300

    Change `_margins` to a Vector3 instead of Vector2
    Margins are added directly to world Bounds, instead of working in screen space, so we may need the Z axis.

--- Runtime/AFovFitter.cs
+++ Runtime/AFovFitter.cs
@@ -8,14 +8,14 @@ namespace Gilzoide.CameraFit
         [Tooltip("Target Camera that will have the FOV adjusted. If null, nothing will happen.")]
         [SerializeField] protected Camera _targetCamera;
 
-        [Tooltip("If true, the FOV will be fit in this object's Start message.")]
+        [Tooltip("If true, the target camera's FOV will be fit in this object's Start message.")]
         [SerializeField] protected bool _applyOnStart = true;
 
-        [Tooltip("If true, the FOV will be fit in this object's Update message.")]
+        [Tooltip("If true, the target camera's FOV will be fit in this object's Update message.")]
         [SerializeField] protected bool _applyOnUpdate = false;
 
         [Tooltip("Margins to add to the bounds when fitting the FOV, in world units.")]
-        [SerializeField] protected Vector2 _margins;
+        [SerializeField] protected Vector3 _margins;
 
         protected abstract Bounds? GetWorldBounds();
 
